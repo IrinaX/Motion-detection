e1b8a04e188cd824f1ade7dbb2eed4756726967f
Iksanova Irina <irka0899@gmail.com>
2022-06-18 01:47:59 +0500

    .

--- main.py
+++ main.py
@@ -10,12 +10,13 @@ while video.isOpened():
     # if we have reached the end of the video we leave
     if not grabbed:
         break
-    # find a difference between tho frames using absdiff
-    diff = cv2.absdiff(frame1, frame2)
     # convert difference to grayscale
-    gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
+    gray1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
+    gray2 = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
+    # find a difference between two frames using absdiff
+    diff = cv2.absdiff(gray1, gray2)
     # apply smoothing to remove noise
-    blur = cv2.GaussianBlur(gray, (5, 5), 0)
+    blur = cv2.GaussianBlur(diff, (5, 5), 0)
     # apply threshold
     _, thresh = cv2.threshold(blur, 70, 255, cv2.THRESH_BINARY)
     # expand the threshold to cover holes
@@ -24,9 +25,10 @@ while video.isOpened():
     contours, _ = cv2.findContours(dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     # Loop over each contour
     for contour in contours:
-        # Calc bounding box
-        x, y, w, h = cv2.boundingRect(contour)
+
         if cv2.contourArea(contour) > 300:
+            # Calc bounding box
+            x, y, w, h = cv2.boundingRect(contour)
             # Draw rectangle - bounding box
             cv2.rectangle(frame1, (x, y), (x + w, y + h), (0, 0, 255), 2)
 
